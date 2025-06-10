258a11feadbbff0a0cc4231a7a0f0422ac66e07c
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2025-06-10 08:20:59 -0300

    feat: adicionar componente de mapa e integrar dependências do Google Maps

---
+++ src/app/components/MapComponent.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React from 'react';
+import { GoogleMap, LoadScript, Marker, DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '70vh'
+};
+
+const center = {
+    lat: -22.8714018,
+    lng: - 42.689089
+}
+const origin = {
+  lat: 37.437041393899676,
+  lng: -4.191635586788259
+};
+
+const destination = {
+  lat: 37.440575591901045,
+  lng: -4.231433159434073
+};
+
+const GoogleMapRouteComponent = () => {
+  const [directions, setDirections] = React.useState(null);
+  const [travelTime, setTravelTime] = React.useState(null);
+
+  const directionsCallback = (response) => {
+    if (response !== null) {
+      if (response.status === 'OK') {
+        setDirections(response);
+        const route = response.routes[0].legs[0];
+        setTravelTime(route.duration.text);
+      } else {
+        console.error('Directions request failed due to ' + response.status);
+      }
+    }
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default GoogleMapRouteComponent;
